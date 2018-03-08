6cda3d90f9bef0d045f0d1de04e8c2ed6645026e
Taylor Bentley <yogasangerin@gmail.com>
2018-03-08 10:02:45 -0600

    update

---
+++ 07week/controlledinput/controlledinput/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      inputValue: '',
+    }
+  }
+
+handleInputChange = (e) => {
+  this.setState({inputValue: e.target.value})
+}
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <input></input><button>Submit</button>
+      </div>
+    );
+  }
+}
+
+export default App;
